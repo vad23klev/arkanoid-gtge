e4f5896b1dca9ae1a1f18bafe4ddc4443a2c90b0
Vadim Klevtsov <vad23klev@gmail.com>
2014-10-21 13:34:25 +0400

    Исправлены типы данных используемые в функциях класса GameObjectView.

--- src/View/GameObjectView.java
+++ src/View/GameObjectView.java
@@ -1,27 +1,35 @@
 package View;
 
+import com.golden.gamedev.object.SpriteGroup;
+
 /**
  * Created by vadim on 10/8/14.
  */
 public abstract class GameObjectView {
 
+    protected SpriteGroup sprites;
+
     public void destroy() {
 
     }
 
-    public float[] getPosition() {
+    public double[] getPosition() {
+        double [] p = new double[2];
+        p[0] = sprites.getSprites()[0].getX();
+        p[1] = sprites.getSprites()[0].getY();
+        return p;
+    }
+
+    public void setPosition(double[] position) {
+
+    }
+
+    public double[] getSpead() {
+
         return null;
     }
 
-    public void setPosition(float[] position) {
-
-    }
-
-    public float[] getSpead() {
-        return null;
-    }
-
-    public void setSpead(float[] spead) {
+    public void setSpead(double[] spead) {
 
     }
 }
