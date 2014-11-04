920fcdddd1a96b508a96b1cdf0258bf13ef91372
Nikita Vladimirovich Nikitsky <nikitskynikita@gmail.com>
2014-11-04 22:08:27 +0300

    Добавлен конструктор класса Brick.

--- src/Model/Brick.java
+++ src/Model/Brick.java
@@ -1,9 +1,17 @@
 package Model;
 
+import Physics.*;
+import View.BrickView;
+
 /**
  * Created by vadim on 10/8/14.
  */
 public class Brick extends GameObjectModel{
+
+    public Brick(Position position) {
+        this.mediator = new Mediator();
+        this.view = new BrickView();
+    }
 
     @Override
     public void CollisionWith(Ball opponent, int side) {
