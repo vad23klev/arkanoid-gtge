246d60f127a3a20627a7c548d1dc314c0f91a649
Nikita Vladimirovich Nikitsky <nikitskynikita@gmail.com>
2014-11-04 21:21:46 +0300

    Добавлен физический класс Position.
    Класс для хранения позиции.

---
+++ src/Physics/Position.java
@@ -0,0 +1,23 @@
+package Physics;
+
+/**
+ * Created by Nikita on 11/4/2014.
+ */
+public class Position {
+
+    /** Позиция по оси x. */
+    public final double X;
+
+    /** Позиция по оси y. */
+    public final double Y;
+
+    /**
+     * Конструктор
+     * @param x - координата по оси x.
+     * @param y - координата по оси y.
+     */
+    public Position(double x, double y) {
+        this.X = x;
+        this.Y = y;
+    }
+}
