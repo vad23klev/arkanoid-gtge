6c18cd8b8d445452726489ccad40079422117fdd
Nikita Vladimirovich Nikitsky <nikitskynikita@gmail.com>
2014-11-04 13:26:42 +0300

    Изменен интерфейс OnCollisionWith.
    Изменен интерфейс для взаимодействия объектов модели.

--- src/Model/OnCollisionWith.java
+++ src/Model/OnCollisionWith.java
@@ -4,9 +4,8 @@ package Model;
  * Created by vadim on 10/8/14.
  */
 public interface OnCollisionWith {
-
-    public void CollisionWithBrick(GameObjectModel opponent, int side);
-    public void CollisionWithBall(GameObjectModel opponent, int side);
-    public void CollisionWithRacket(GameObjectModel opponent, int side);
-    public void CollisionWithRoy(GameObjectModel opponent, int side);
+    public void CollisionWith(Brick opponent, int side);
+    public void CollisionWith(Ball opponent, int side);
+    public void CollisionWith(Racket opponent, int side);
+    public void CollisionWith(Roy opponent, int side);
 }
