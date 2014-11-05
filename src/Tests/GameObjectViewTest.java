7f6e078c7da75640e12865b118681000a46d222b
Vadim Klevtsov <vad23klev@gmail.com>
2014-11-05 03:40:03 +0300

    Добавлен тестовый класс для GameObjectView.

---
+++ src/Tests/GameObjectViewTest.java
@@ -0,0 +1,24 @@
+package Tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameObjectViewTest {
+
+
+    @Test
+    public void testSetPosition() throws Exception {
+
+    }
+
+    @Test
+    public void testSetSpead() throws Exception {
+
+    }
+
+    @Test
+    public void testIsMySprite() throws Exception {
+
+    }
+}
