098f1d85fed9a43409b934817a9b823485d0cb7c
Vadim Klevtsov <vad23klev@gmail.com>
2014-11-05 04:34:15 +0300

    добавлены тесты для роя.

--- src/Tests/RoyTest.java
+++ src/Tests/RoyTest.java
@@ -5,5 +5,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class RoyTest {
+    @Test
+    /** Тест на добавление элемента в рой */
+    public void Add_element() throws Exception {
 
+    }
+
+    @Test
+    /** Тест на объединение 2 роев. */
+    public void Merge_1_roy() throws Exception {
+
+    }
+
+    @Test
+    /** Тест на объединение 3S роев. */
+    public void Merge_2_roy() throws Exception {
+
+    }
 }
