96cb1d518adda385716b289e35226a4661dcbad4
Vadim Klevtsov <vad23klev@gmail.com>
2014-11-05 03:41:10 +0300

    Добавлен тестовый класс для GameView.

---
+++ src/Tests/GameViewTest.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameViewTest {
+
+    @Test
+    public void testFind_view() throws Exception {
+
+    }
+}
