af6af5ac3ccc3c5644db5779ee62de2571f5d1ff
Vadim Klevtsov <vad23klev@gmail.com>
2014-11-05 20:48:56 +0300

    добавлены методы в класс роя(добавление элемента и соединение роев)

--- src/Model/Roy.java
+++ src/Model/Roy.java
@@ -13,8 +13,29 @@ public class Roy  extends GameObjectModel {
      * Конструктор.
      * @param position - позиция роя в начальный момент времени.
      */
+    final int MAX_ELEMENT_COUNT = 3;
+    /**
+     * Конструктор.
+     * @param position - позиция роя в начальный момент времени.
+     */
     public Roy(Position position) {
         this.mediator = new Mediator();
         this.view = new RoyView();
+    }
+
+    /**
+     * Слияние двух и более роев в 1.
+     * @param others - массив роев для слияния с текущим.
+     */
+    public void Merge(Roy[] others) {
+
+    }
+
+    /**
+     * Конструктор.
+     * @param element - элемент попадающий в рой.
+     */
+    public void Append(GameObjectModel element) {
+
     }
 }
