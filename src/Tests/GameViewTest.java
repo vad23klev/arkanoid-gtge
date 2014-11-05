baa1e6abd197dc26a84585f1431c504c42aca8f5
Vadim Klevtsov <vad23klev@gmail.com>
2014-11-05 04:11:29 +0300

    Добавлены тесты для класса GameView.

--- src/Tests/GameViewTest.java
+++ src/Tests/GameViewTest.java
@@ -7,7 +7,14 @@ import static org.junit.Assert.*;
 public class GameViewTest {
 
     @Test
-    public void testFind_view() throws Exception {
+    /** Тест поиск представления по спрайту(спрайт принадлежит представлению). */
+    public void Find_view_sprite_in_view() throws Exception {
+
+    }
+
+    @Test
+    /** Тест поиск представления по спрайту(свободный спрайт). */
+    public void Find_view_sprite_without_view() throws Exception {
 
     }
 }
