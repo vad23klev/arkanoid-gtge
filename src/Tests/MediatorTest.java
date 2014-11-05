13c8b597d8223f34d0cac7db562e662e7adae0a8
Vadim Klevtsov <vad23klev@gmail.com>
2014-11-05 04:28:19 +0300

    добавлены тесты для посредника.

---
+++ src/Tests/MediatorTest.java
@@ -0,0 +1,44 @@
+package Tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MediatorTest {
+
+    @Test
+    /** Тест коллизии шара с ракеткой. */
+    public void Hit_ball_racket() throws Exception {
+
+    }
+
+    @Test
+    /** Тест коллизии шара с кирпичем. */
+    public void Hit_ball_brick() throws Exception {
+
+    }
+
+    @Test
+    /** Тест коллизии шара с двумя кирпичами. */
+    public void Hit_ball_2_brick() throws Exception {
+
+    }
+
+    @Test
+    /** Тест коллизии шара с роем. */
+    public void Hit_ball_roy() throws Exception {
+
+    }
+
+    @Test
+    /** Тест коллизии шара с двумя роями. */
+    public void Hit_ball_2_roy() throws Exception {
+
+    }
+
+    @Test
+    /** Тест коллизии шара с кирпичем и роем. */
+    public void Hit_ball_roy_brick() throws Exception {
+
+    }
+}
