42d12a6c3c1d316e1a17331672d62bbf3a482fe6
Vadim Klevtsov <vad23klev@gmail.com>
2014-11-05 04:44:51 +0300

    добавлены тесты для доски.

--- src/Tests/BoardTest.java
+++ src/Tests/BoardTest.java
@@ -5,5 +5,22 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BoardTest {
+    @Test
+    /** Тест на удаление элемента  */
+    public void Remove() throws Exception {
+
+    }
+
+    @Test
+    /** Тест на конец игры(конец)*/
+    public void End_game_positive() throws Exception {
+
+    }
+
+    @Test
+    /** Тест на конец игры(игра продолжается)*/
+    public void End_game_negative() throws Exception {
+
+    }
 
 }
