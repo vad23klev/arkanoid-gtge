bd63685a154fd1287f3e8ecfc503ae060d7384d9
Vadim Klevtsov <vad23klev@gmail.com>
2014-11-19 20:45:14 +0300

    Добавлен заголовок теста для уничтожения меча

--- src/Tests/BrickTest.java
+++ src/Tests/BrickTest.java
@@ -5,5 +5,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BrickTest {
-
+    @Test
+    /** Тест на удаление кирпича. */
+    public void destroyBrick() throws Exception {
+    }
 }
