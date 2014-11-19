f3a311125e58181f90d7bc8cc0b0b3ccb811239b
Nikita Nikitsky <nikitskynikita@gmail.com>
2014-11-19 17:26:15 +0300

    реализованы оставшиеся тесты для класса Ball

--- src/Tests/BallTest.java
+++ src/Tests/BallTest.java
@@ -1,21 +1,39 @@
 package Tests;
 
 import Model.Ball;
+import Model.Board;
+import Model.Racket;
+import Model.Roy;
 import Physics.Direction;
 import Physics.Position;
 import Physics.Speed;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Assert;
 
 public class BallTest {
 
     private Ball ball;
 
+    private Racket racket;
+
+    private Roy roy;
+
+    private Board board;
+
     @Before
     public void ball() {
         this.ball = new Ball(new Position(0,0));
     }
+
+    @Before
+    public void racket() { this.racket = new Racket(new Position(0,0)); }
+
+    @Before
+    public void roy() { this.roy = new Roy(new Position(0,0)); }
+
+    @Before
+    public void board() { this.board = new Board(); }
 
     @Test
     /** Тест обобщенных методов, точка касания сверху шара. */
@@ -92,25 +110,49 @@ public class BallTest {
     @Test
     /** Тест коллизии шара и ракетки, точка касания по центру ракетки. */
     public void CenterCollisionWithRacket() throws Exception {
+        ball();
+        racket();
+        ball.setSpeed(new Speed(1, 1));
+        ball.CollisionWith(racket, Direction.Bottom());
+        Assert.assertTrue("Speed not equal. Center collision(Ball)", ball.getSpeed().equals(new Speed(1, -1)));
     }
 
     @Test
     /** Тест коллизии шара и ракетки, точка касания слевой стороны ракетки.. */
     public void LeftCollisionWithRacket() throws Exception {
+        ball();
+        racket();
+        ball.setSpeed(new Speed(1, 1));
+        ball.CollisionWith(racket, Direction.Bottom());
+        Assert.assertTrue("Speed not equal. Left collision(Ball)", ball.getSpeed().equals(new Speed(1, -1)));
     }
 
     @Test
     /** Тест коллизии шара и ракетки, точка касания справой стороны ракетки. */
     public void RightCollisionWithRacket() throws Exception {
+        ball();
+        racket();
+        ball.setSpeed(new Speed(1, 1));
+        ball.CollisionWith(racket, Direction.Bottom());
+        Assert.assertTrue("Speed not equal. Right collision(Ball)", ball.getSpeed().equals(new Speed(1, -1)));
     }
 
     @Test
     /** Тест коллизии шара и роя */
     public void CollisionWithRoy() throws Exception {
+        ball();
+        roy();
+        ball.setSpeed(new Speed(1,1));
+        Assert.assertTrue("Ball not destroy.", ball.isDestroy());
     }
 
     @Test
     /** Тест коллизии шара и края доски, точка касания снизу шара */
     public void CollisionWithBottomBoard() throws Exception {
+        ball();
+        board();
+        ball.setSpeed(new Speed(1,1));
+        ball.hit(Direction.Bottom());
+        Assert.assertTrue("Ball not destroy.", ball.isDestroy());
     }
 }
