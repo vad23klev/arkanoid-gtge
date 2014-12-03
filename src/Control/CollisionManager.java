5075e464a4937b248290ac238661b4a519f54aae
Vadim Klevtsov <vad23klev@gmail.com>
2014-12-03 19:32:18 +0300

    Медиатор перенесен в обработчик коллизий

--- src/Control/CollisionManager.java
+++ src/Control/CollisionManager.java
@@ -1,15 +1,26 @@
 package Control;
 
+import Model.Mediator;
+
 /**
  * Класс для обнаружения коллизий.
  * Created by vadim on 10/8/14.
  */
 public class CollisionManager {
 
+    /* Связь обработчика коллизий и модели*/
+    protected Mediator mediator= null;
+
     /**
      * Проверка на коллизии.
      */
     public void check_collision() {
 
+    }
+    /**
+     * Конструктор.
+     */
+    public CollisionManager(Mediator mediator) {
+        this.mediator = mediator;
     }
 }
