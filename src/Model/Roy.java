37f5d442d3db878393383551f786c7852b62ca27
Vadim Klevtsov <vad23klev@gmail.com>
2014-12-03 21:18:38 +0300

    Реализованы недостоющие методы модели роя

--- src/Model/Roy.java
+++ src/Model/Roy.java
@@ -17,7 +17,10 @@ public class Roy  extends GameObjectModel {
      */
     public Roy(Position position) {
         this.elements = new RoyElement[MAX_ELEMENT_COUNT];
-        this.view = new RoyView();
+        for (int i = 0; i < MAX_ELEMENT_COUNT; i++) {
+            this.elements[i] = null;
+        }
+        this.view = new RoyView(this, position);
     }
 
     /**
@@ -25,6 +28,17 @@ public class Roy  extends GameObjectModel {
      * @param others - массив роев для слияния с текущим.
      */
     public void Merge(Roy[] others) {
+        for (int i = 0; i < others.length; i++ ) {
+            for (int j = 0; j < others[i].getElements().length; j++) {
+                if (!CheckOverflow()) {
+                    this.Append(others[i].getElements()[j]);
+                } else {
+                    j = others[i].getElements().length;
+                    i = others.length;
+                }
+            }
+            others[i].destroy();
+        }
 
     }
 
@@ -33,13 +47,31 @@ public class Roy  extends GameObjectModel {
      * @param element - элемент попадающий в рой.
      */
     public void Append(GameObjectModel element) {
-
+        if(!this.CheckOverflow()) {
+            for (int i = 0; i < MAX_ELEMENT_COUNT; i++) {
+                if (elements[i] == null) {
+                    elements[i] = new RoyElement(this, element.getPosition());
+                }
+            }
+        }
     }
 
     /**
      * Проверка переполнения роя.
      */
     public boolean CheckOverflow() {
+        if(elements[MAX_ELEMENT_COUNT -1] != null) {
+            this.destroy();
+            return true;
+        }
         return false;
+    }
+
+    /**
+     * Возвращаем эелементы роя.
+     * @return - массив элементов роя.
+     */
+    public RoyElement[] getElements() {
+        return this.elements;
     }
 }
