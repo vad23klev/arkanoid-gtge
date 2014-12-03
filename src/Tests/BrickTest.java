da1441aedb3b56200953e3cc1a57ecab035fec39
Vadim Klevtsov <vad23klev@gmail.com>
2014-12-03 19:33:47 +0300

    Добавлен тест на разрушение кирпича

--- src/Tests/BrickTest.java
+++ src/Tests/BrickTest.java
@@ -1,5 +1,9 @@
 package Tests;
 
+import Model.Brick;
+import Physics.Position;
+import View.GameObjectView;
+import View.GameView;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,5 +12,10 @@ public class BrickTest {
     @Test
     /** Тест на удаление кирпича. */
     public void destroyBrick() throws Exception {
+        Brick brick = new Brick(new Position(0,0));
+        GameView view = new GameView();
+        GameObjectView bview = brick.view();
+        brick.destroy();
+        assertTrue("Brick destroy test is failed",view.findModelByView(bview) == null);
     }
 }
