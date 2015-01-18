ea765949a7f14814d261e8b4a9bd6fde9d6ddba8
Vadim Klevtsov <vad23klev@gmail.com>
2015-01-18 15:28:58 +0300

    Исправлен класс GameAdvanceCollisionGroup. Дополнен метод collided.

--- src/Control/CollisionControl/GameAdvanceCollisionGroup.java
+++ src/Control/CollisionControl/GameAdvanceCollisionGroup.java
@@ -1,5 +1,8 @@
 package Control.CollisionControl;
 
+import Game.Arkanoid;
+import Model.GameObjectModel;
+import Physics.Direction;
 import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.collision.AdvanceCollisionGroup;
 
@@ -11,6 +14,9 @@ public class GameAdvanceCollisionGroup extends AdvanceCollisionGroup {
 
     @Override
     public void collided(Sprite sprite, Sprite sprite2) {
+        GameObjectModel model = Arkanoid.board.findModelBySprite(sprite);
+        GameObjectModel model2 = Arkanoid.board.findModelBySprite(sprite2);
+        Arkanoid.mediator.hit(model, model2, Direction.Top());
 
     }
 }
