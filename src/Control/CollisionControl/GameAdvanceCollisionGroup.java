4b8260bb16333f4e98d6bace1ac9996204f2abb9
Vadim Klevtsov <vad23klev@gmail.com>
2015-01-19 08:23:16 +0300

    В обработчик коллизий мяча с объектами игры, добавлено определение направления удара.

--- src/Control/CollisionControl/GameAdvanceCollisionGroup.java
+++ src/Control/CollisionControl/GameAdvanceCollisionGroup.java
@@ -16,6 +16,13 @@ public class GameAdvanceCollisionGroup extends AdvanceCollisionGroup {
     public void collided(Sprite sprite, Sprite sprite2) {
         GameObjectModel model = Arkanoid.board.findModelBySprite(sprite);
         GameObjectModel model2 = Arkanoid.board.findModelBySprite(sprite2);
+        Direction direction;
+        if (model.getPosition().Y+20 > model2.getPosition().Y) {
+            if (model.getPosition().X <= model2.getPosition().X) {
+                direction = Direction.Left();
+            } else { direction = Direction.Right();}
+        } else { direction = Direction.Top();}
+
         Arkanoid.mediator.hit(model, model2, Direction.Top());
 
     }
